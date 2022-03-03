982e56fd6ac22dfec13333691094b833428f6770
henry cunha <henrycunh@gmail.com>
2022-03-02 21:46:43 -0300

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kleur-template
 🌈 Add color to your strings with templating
+
+## usage
+```bash
+yarn install kleur-template
+```
+
+```js
+import { style } from 'kleur-template'
+
+console.log(style`Hello [world](green.underline)`)
+```
+
+Check [kleur](https://github.com/lukeed/kleur#api) for styling options :]
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import kleur from 'kleur'
+
+export function useStyle (content: string) {
+    const replaceWithEffects = (match, content, effectChain) => {
+        if (!effectChain.length || !content) return match
+        const effects = effectChain.split('.')
+        return effects.reduce((acc, effect) => {
+            const effectFn = kleur[effect]
+            if (!effectFn) return content
+            return effectFn(acc)
+        }, content)
+    }
+    return content.replace(/\[([^\]]*?)\]\(([^\)]*?)\)/g, replaceWithEffects)
+}
+
+export function style (tokens: TemplateStringsArray, ...args: string[]) {
+    const combined = tokens.reduce(
+        (acc, curr, i) => `${acc}${curr}${args[i] || ''}`,
+        ''
+    )
+    return useStyle(combined)
+}
+
+
+
+// console.log(
+//     color`[Hello world](green.underline) teste`,
+//     `${kleur.green().underline('Hello world')}`
+// )
